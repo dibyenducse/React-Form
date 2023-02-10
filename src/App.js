3d1dd12ef7239674ddb4026a1842cedc03640cbe
Dibyendu Mondal <dibyendu.cse.ru@gmail.com>
2023-02-10 12:10:34 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-export default function Form({
-  // Try 'submitting', 'error', 'success':
-  status = 'f'
-}) {
-  if (status === 'success') {
+export default function Form({status = 'empty'}){
+  
+  if (status === 'success'){
     return <h1>That's right!</h1>
   }
   return (
     <>
-      <h2>City quiz</h2>
+      <h2>City Quiz</h2>
       <p>
-        In which city is there a billboard that turns air into drinkable water?
+        In which city is there a death river?
       </p>
       <form>
-        <textarea disabled={
-          status === 'submitting'
-        } />
+        <texarea disabled ={status ==='submitting'} />
         <br />
-        <button disabled={
-          status === 'empty' ||
-          status === 'submitting'
-        }>
-          Submit
-        </button>
-        {status === 'error' &&
-          <p className="Error">
-            Good guess but a wrong answer. Try again!
-          </p>
+        <button disabled ={ status ==='empty' || status === 'submitting'
+        } /> submit </button>
+        {status === 'error' && 
+        <p className='Error'>
+          Good guess but a wrong answer. Try again!
+        </p>
         }
       </form>
-      </>
+    </>
   );
 }
